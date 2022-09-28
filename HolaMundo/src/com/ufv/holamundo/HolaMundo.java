ddb239364ab7150b5e5ce2c524634070a6d5f3a9
Gian08922 <giancarloriosaltamirano@hotmail.com>
2022-09-28 20:54:40 +0200

    Commit Inicial

---
+++ HolaMundo/src/com/ufv/holamundo/HolaMundo.java
@@ -0,0 +1,10 @@
+package com.ufv.holamundo;
+
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("\033[36mHola Mundo\u001B[0m");
+        System.out.println("\033[35mHola bb");
+
+    }
+}
